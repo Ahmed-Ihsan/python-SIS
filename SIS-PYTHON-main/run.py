a816adfa29c0e2062420e29f643236869ddefe97
Ahmed Ihsan Ali <46008612+Ahmed-Ihsan@users.noreply.github.com>
2021-02-12 19:34:33 +0300

    Update run.py

--- SIS-PYTHON-main/run.py
+++ SIS-PYTHON-main/run.py
@@ -92,7 +92,7 @@ def route_page(Type_path=None):
 						db.session.add(us)
 						db.session.commit()
 						return render_template('admin_Send_mail.html')
-				elif Type_path in "add":
+				elif Type_path == "add":
 					name1=request.form['name1']
 					name2=request.form['name2']
 					Direct_Date = request.form['date_time']
