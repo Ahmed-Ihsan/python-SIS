914f71087cf312ed7a91a0501741303bc576f6cc
Ahmed Ihsan Ali <46008612+Ahmed-Ihsan@users.noreply.github.com>
2021-03-20 02:08:53 +0300

    Add files via upload

--- SIS-PYTHON-main/create_db.py
+++ SIS-PYTHON-main/create_db.py
@@ -11,17 +11,26 @@ def PassWord(string):
 	
 def main():
 	db.create_all()
-	us=User(username='ahmed0' , password=generate_password_hash(PassWord("admin")) , department='adding'  , level="رئيس فسم"  ,le_el="1234"  )
-	db.session.add(us)
-	db.session.commit()
-	us=User(username='ahmed1' , password=generate_password_hash(PassWord("admin")) , department='admin'  , level="amdin"  ,le_el="----" )
-	db.session.add(us)
-	db.session.commit()
-	us=User(username='ahmed2' , password=generate_password_hash(PassWord("admin")) , department='هندسة تقنيات الحاسوب'  , level="رئيس فسم"  ,le_el="1234" )
-	db.session.add(us)
-	db.session.commit()
-	us=User(username='ahmed3' , password=generate_password_hash(PassWord("admin")) , department='Administrative_unit'  , level="رئيس فسم"  ,le_el="1234" )
-	db.session.add(us)
+	array =['adding','هندسة تقنيات الحاسوب','Administrative_unit','هندسة الاجهزة الطبية','طب الاسنان','لغة العربية','قانون','تحليلات']
+	for n in array:
+		us11=Level( department= n  , level="رئيس فسم"  ,le_el="1234" )
+		db.session.add(us11)
+		us21=Level( department= n  , level="مقرر قسم"  ,le_el="123" )
+		db.session.add(us21)
+		us31=Level( department= n , level="استاذ"  ,le_el="12" )
+		db.session.add(us31)
+		us41=Level( department= n  , level="موظف " ,le_el="1")
+		db.session.add(us41)
+	us411=Level( department= "admin"  , level="admin " ,le_el="1234")
+	db.session.add(us411)
+	us1=User(username='ahmed0' , password=generate_password_hash(PassWord("admin")) , Level_id="1")
+	db.session.add(us1)
+	us2=User(username='ahmed1' , password=generate_password_hash(PassWord("admin")) , Level_id="33")
+	db.session.add(us2)
+	us3=User(username='ahmed2' , password=generate_password_hash(PassWord("admin")) , Level_id="5")
+	db.session.add(us3)
+	us4=User(username='ahmed3' , password=generate_password_hash(PassWord("admin")), Level_id="9")
+	db.session.add(us4)
 	db.session.commit()
 	
 if __name__ == "__main__":
--- SIS-PYTHON-main/run.py
+++ SIS-PYTHON-main/run.py
@@ -1,11 +1,13 @@
 import os
 import time
-from flask_sqlalchemy import SQLAlchemy
+import string
+import random
+from flask_sqlalchemy import SQLAlchemy 
 from flask import Flask, render_template, request, redirect, url_for, flash ,send_from_directory ,session
 from werkzeug.security import check_password_hash , generate_password_hash
 from model import *
 from datetime import datetime
-#from flask_wtf.csrf import CSRFProtect
+from flask_wtf.csrf import CSRFProtect
 from flask_sslify import SSLify
 
 def PassWord(string):
@@ -13,6 +15,21 @@ def PassWord(string):
 	for i in string:
 		new_pass=new_pass+str(hex(ord(i)))
 	return str(new_pass)
+
+def password_generator():
+
+    LETTERS = string.ascii_letters
+    NUMBERS = string.digits  
+    #PUNCTUATION = string.punctuation  
+
+    printable = f'{LETTERS}{NUMBERS}'#{PUNCTUATION}
+
+    printable = list(printable)
+    random.shuffle(printable)
+
+    random_password = random.choices(printable, k=8)
+    random_password = ''.join(random_password)
+    return random_password
 
 UPLOAD_FOLDER = os.path.join('static', 'upload_file')
 app = Flask(__name__)
@@ -32,7 +49,30 @@ def route_section(Type_path=None):
 	if  not ('username' in session):
 		return redirect(url_for('login'))
 	elif request.method == 'POST':
-		if Type_path in "add":
+		if Type_path == "sendMail":
+			a=['adding','هندسة تقنيات الحاسوب','Administrative_unit','هندسة الاجهزة الطبية','طب الاسنان','لغة العربية','قانون','تحليلات']
+			a.remove(session['department'])
+			f = request.files['file']
+			if f != None:
+				f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
+				save_file=os.path.join(app.config['UPLOAD_FOLDER'],f.filename)
+				From=request.form['From']
+				name=request.form['name']
+				time=datetime.now()
+				us1=[]
+				for i in a:
+					try:
+						f1 = request.form[i]
+					except:
+						f1= None
+					if f1 != None:
+						us=mail(path=save_file, name_file=f.filename , name=name , Direct_Date=time , To=f1 , From=From )
+						us1.append(us)
+				for i in us1:
+					db.session.add(i)
+				db.session.commit()
+				return render_template('section_send.html', data=a ,qsm=session['department'])
+		elif Type_path in "add":
 			name1=request.form['name1']
 			name2=request.form['name2']
 			Direct_Date = request.form['date_time']
@@ -41,34 +81,37 @@ def route_section(Type_path=None):
 			address=request.form['address']
 			department=request.form['department']
 			level=request.form['level']
-			print(int(Direct_Date[1:4]))
 			Direct_Date=datetime(int(Direct_Date[0:4]),int(Direct_Date[5:7]), int(Direct_Date[8:10]))
 			data=student(firstname=name1 , lestname=name2 , Direct_Date=Direct_Date , Email=email , phonenumber=ph_num , Address=address , department=department , level=level )
 			db.session.add(data)
 			db.session.commit()
-			return render_template('section_add.html')
+			return render_template('section_add.html',db=session['department'])
 		elif Type_path in "received_file":
 			search=request.form['search']
-			data=mail.query.filter_by(To=session['department'] , From=search).all()
-			return render_template('sectionMailR.html',send=False, inf=data)
+			data=mail.query.filter_by(To=session['department'] , From=search).order_by(mail.id.desc()).all()
+			return render_template('sectionMailR.html',send=False, inf=data ,db=session['department'])
 		elif Type_path in "send_file":
 			search=request.form['search']
-			data=mail.query.filter_by(From=session['department'] , To=search).all()
-			return render_template('sectionMailS.html',send=True, inf=data)
+			data=mail.query.filter_by(From=session['department'] , To=search).order_by(mail.id.desc()).all()
+			return render_template('sectionMailS.html',send=True, inf=data ,db=session['department'])
 	elif Type_path != None:
 		if Type_path in "add":
 			return render_template('section_add.html', db=session['department'] )
+		elif Type_path == "sendMail":
+			a=['adding','هندسة تقنيات الحاسوب','Administrative_unit','هندسة الاجهزة الطبية','طب الاسنان','لغة العربية','قانون','تحليلات']
+			a.remove(session['department'])
+			return render_template('section_send.html', data=a ,db=session['department'])
 		elif Type_path in "search":
-			    data2=student.query.all()
+			    data2=student.query.order_by(student.firstname.asc()).all()
 			    return render_template('section_editer.html',info_fil=None,info_all=data2 , deep=session['department'])
 		elif Type_path in "received_file":
-			data=mail.query.filter_by(To=session['department']).all()
-			return render_template('sectionMailR.html',send=False, inf=data)
+			data=mail.query.filter_by(To=session['department']).order_by(mail.id.desc()).all()
+			return render_template('sectionMailR.html',send=False, inf=data ,db=session['department'])
 		elif Type_path in "send_file":
-			data=mail.query.filter_by(From=session['department']).all()
-			return render_template('sectionMailS.html',send=True, inf=data)
+			data=mail.query.filter_by(From=session['department']).order_by(mail.id.desc()).all()
+			return render_template('sectionMailS.html',send=True, inf=data ,db=session['department'])
 	else:
-		data=student.query.filter_by(department=session['department']).all()
+		data=student.query.filter_by(department=session['department']).order_by(student.firstname.asc()).all()
 		return render_template('section_home.html' , data=data , db=session['department'] )
 
 
@@ -78,13 +121,13 @@ def route_page(Type_path=None):
 	if not ('username' in session): 
 		return redirect(url_for('login'))
 	elif "admin" in session['department']:
+		a=['adding','هندسة تقنيات الحاسوب','Administrative_unit','هندسة الاجهزة الطبية','طب الاسنان','لغة العربية','قانون','تحليلات']
 		if Type_path == "addacc":
-				return render_template('admin_add_account.html' ,ret2=" ", ret='successfully')
-		if Type_path == "addacc2":
-				data=User.query.all()
+			return render_template('admin_add_account.html' , ret2=a, ret='successfully')
+		elif Type_path == "addacc2":
+				data=User.query.order_by(User.username.asc()).all()
 				return render_template('admin_sting_account.html',inf=data)
 		elif request.method == 'POST':
-			print(Type_path)
 			if Type_path == "send":
 				f = request.files['file']
 				if f != None:
@@ -92,12 +135,20 @@ def route_page(Type_path=None):
 					save_file=os.path.join(app.config['UPLOAD_FOLDER'],f.filename)
 					From=request.form['From']
 					name=request.form['name']
-					To=request.form['To']
 					time=datetime.now()
-					us=mail(path=save_file, name_file=f.filename , name=name , Direct_Date=time , To=To , From=From )
-					db.session.add(us)
+					us1=[]
+					for i in a:
+						try:
+							f1 = request.form[i]
+						except Exception as e:
+							f1= None
+						if f1 != None:
+							us=mail(path=save_file, name_file=f.filename , name=name , Direct_Date=time , To=f1 , From=From )
+							us1.append(us)
+					for i in us1:
+						db.session.add(i)
 					db.session.commit()
-					return render_template('admin_Send_mail.html')
+					return render_template('admin_Send_mail.html', data=a)
 			elif Type_path == "add":
 				name1=request.form['name1']
 				name2=request.form['name2']
@@ -107,40 +158,38 @@ def route_page(Type_path=None):
 				address=request.form['address']
 				department=request.form['department']
 				level=request.form['level']
-				print(int(Direct_Date[1:4]))
 				Direct_Date=datetime(int(Direct_Date[0:4]),int(Direct_Date[5:7]), int(Direct_Date[8:10]))
 				data=student(firstname=name1 , lestname=name2 , Direct_Date=Direct_Date , Email=email , phonenumber=ph_num , Address=address , department=department , level=level )
 				db.session.add(data)
 				db.session.commit()
-				return render_template('admin_Add_studenat.html')
+				return render_template('admin_Add_studenat.html',inf=a)
 			else:
 				search=request.form['search']
 				Type=request.form['Type']
 				section=request.form['section']
 				if search == "":
-					print(1)
 					if Type in "2":
-						data=mail.query.filter_by(From=section).all()
-						return render_template('admin_sections.html',s=False ,inf=data )
+						data=mail.query.filter_by(From=section).order_by(mail.id.desc()).all()
+						return render_template('admin_sections.html',s=False ,inf=data ,inf2=a )
 					elif Type in "1":
-						data=mail.query.filter_by(To=section).all()
-						return render_template('admin_sections.html', s=False, inf=data )
+						data=mail.query.filter_by(To=section).order_by(mail.id.desc()).all()
+						return render_template('admin_sections.html', s=False, inf=data ,inf2=a)
 					else:
-						data=student.query.filter_by(department=section).all()
-						return render_template('admin_sections.html',s=True, inf=data)
+						data=student.query.filter_by(department=section).order_by(student.firstname.asc()).all()
+						return render_template('admin_sections.html',s=True, inf=data ,inf2=a)
 				else:
-					data=student.query.filter_by(department=section , firstname=search ).all()
-					return render_template('admin_sections.html' ,s=True, inf=data)
+					data=student.query.filter_by(department=section , firstname=search ).order_by(student.firstname.asc()).all()
+					return render_template('admin_sections.html' ,s=True, inf=data )
 		elif Type_path == None:
-			data=mail.query.all()
-			return render_template('admin_sections.html' ,s=False, inf=data )
+			data=mail.query.order_by(mail.id.desc()).all()
+			return render_template('admin_sections.html' ,s=False, inf=data ,inf2=a )
 		elif Type_path in "mail":
-			data=mail.query.filter_by(To=Type_path).all()
-			return render_template('admin_Mail.html', inf=data )
+			data=mail.query.filter_by(To=Type_path).order_by(mail.id.desc()).all()
+			return render_template('admin_Mail.html', inf=data ,inf2=a)
 		elif Type_path in "send":
-			return render_template('admin_Send_mail.html')
+			return render_template('admin_Send_mail.html',data=a)
 		else:
-			return render_template('admin_Add_studenat.html')
+			return render_template('admin_Add_studenat.html',inf2=a)
 	else:
 		return redirect(url_for('login'))
 
@@ -154,17 +203,17 @@ def route_Administrative(Type_path=None):
 		if Type_path != None and (Type_path != "sends" and Type_path != "send") :
 			if "mailRs" == Type_path :
 				search=request.form['search']
-				data=mail.query.filter_by(To=session['department'] , From=search).all()
+				data=mail.query.filter_by(To=session['department'] , From=search).order_by(mail.id.desc()).all()
 				return render_template('Administrative_mailR.html', s=False, inf=data )
 			elif "mailSs" == Type_path :
 				search=request.form['search']
-				data=mail.query.filter_by(From=session['department'] , To=search).all()
+				data=mail.query.filter_by(From=session['department'] , To=search).order_by(mail.id.desc()).all()
 				return render_template('Administrative_mailR.html', s=True, inf=data )
 			elif "mailS" == Type_path :
-				data=mail.query.filter_by(From=session['department']).all()
+				data=mail.query.filter_by(From=session['department']).order_by(mail.id.desc()).all()
 				return render_template('Administrative_mailS.html', s=True, inf=data )
 			elif "mailR" == Type_path :
-				data=mail.query.filter_by(To=session['department']).all()
+				data=mail.query.filter_by(To=session['department']).order_by(mail.id.desc()).all()
 				return render_template('Administrative_mailR.html', s=False, inf=data )
 			else:
 				return redirect(url_for('route_Administrative'))
@@ -461,12 +510,17 @@ def login():
 	if request.method == 'POST':
 		try:
 			username=request.form['username']
+			newpassword=request.form['newpassword']
 			data=User.query.filter_by(username = username ).first()
-			info=[data.id,data.username,data.department]
 			password=request.form['password']
 			if check_password_hash(data.password ,PassWord(password)):
+				if newpassword != "1" :
+					data_up2 = generate_password_hash(PassWord(newpassword))
+					exec(open('admin.file/upload.py').read())
 				session['username']= username
+				data=Level.query.filter_by(id = data.Level_id ).first()
 				session['department']= data.department
+				session['level']= data.le_el
 				if session['department'] in "adding":
 					return redirect(url_for('route_uint'))
 				elif session['department'] in "admin":
@@ -475,18 +529,6 @@ def login():
 					return redirect(url_for('route_Administrative'))
 				else:
 					return redirect(url_for("route_section"))
-				#if data.department == "admin":
-					#return redirect(url_for('admin'))
-				#elif data.department == 'Administrative_unit':
-					#return redirect(url_for('Administrative_unit'))
-				#elif data.department == 'unitname':
-					#count_s=student.query.count()
-					#count_t=teacher.query.count()
-					#count_d=Department.query.count()
-					#count_all=[count_s , count_t , 7]
-					#return render_template('stated.html', info=info , count=count_all)
-				#elif data.department == 'adding':
-					#return render_template('Add_student.html', dep=None )
 			errer='errer in passsword or usear name'
 			return render_template('login_page.html' ,e=errer)
 		except Exception as e:
@@ -497,61 +539,49 @@ def login():
 @app.route("/admin", methods=['GET', 'POST'])
 def admin():
 	if 'username' in session:
+		a=['adding','هندسة تقنيات الحاسوب','Administrative_unit','هندسة الاجهزة الطبية','طب الاسنان','لغة العربية','قانون','تحليلات']
 		if request.method == 'POST':
 			try:
 				username=request.form['username']
-				data=request.form['data']
+				data=request.form['password']
 				typ=request.form['type']
 				le_el=request.form['le_el']
 				department=request.form['department']
 				if 'insert' in typ:
-					level = " "
+					try:
+						a1=a.index(department)+1 
+						a1=a1*4
+					except Exception as e :
+						return render_template('admin_add_account.html' ,ret2=a, e=e ,ret="")
 					if le_el == '1234':
-						level='رئيس فسم'
+						Level_id= a1-3
 					elif le_el == '123' :
-						level='مقرر القسم'
+						Level_id= a1-2
 					elif le_el == '12':
-						level == 'موظف'	
+						Level_id = a1-1	
 					else:
-						level = 'زائر'					
-					us=User(username=username , password=generate_password_hash(PassWord(data)), department=department ,level=level, le_el=le_el)
+						Level_id = a1	
+					password = 	password_generator()		
+					us=User(username=username , password=generate_password_hash(PassWord(password)),Level_id=Level_id)
 					db.session.add(us)
 					db.session.commit()
-					return render_template('admin_add_account.html' ,ret2="", ret='successfully')
+					return render_template('admin_add_account.html' ,ret2= a, ret = [password,username] )
 				elif 'upload' in typ :
 					data_up2 = generate_password_hash(PassWord(data))
 					exec(open('admin.file/upload.py').read())
-					return render_template('admin_add_account.html' ,ret2="", ret='successfully')
+					return render_template('admin_add_account.html' ,ret2=a, ret='successfully')
 				elif 'delete' in typ:
 					data_dl = username
 					exec(open('admin.file/delete.py').read())
-					return render_template('admin_add_account.html' ,ret2="", ret='successfully')
+					return render_template('admin_add_account.html' ,ret2=a, ret='successfully')
 				elif 'search' in typ:
 					data = User.query.filter_by(username=username).all()
-					return render_template('admin_add_account.html',ret2=data , ret='successfully')
+					return render_template('admin_add_account.html',ret2=a , ret='successfully')
 			except Exception as e:
-				return render_template('admin_add_account.html' ,ret2="", e=e)
+				return render_template('admin_add_account.html' ,ret2=a, e=e , ret = 'a')
 		return redirect(url_for('login'))
 	return redirect(url_for('login'))
 
-'''@app.route("/upload_file", methods=['GET', 'POST'])
-def upload():
-	if 'username' in session:
-	if request.method == 'POST':
-		try:
-			f = request.files['file']
-			debartment = request.form['debartment']
-			if f != None:
-				f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
-				save_file=os.path.join(app.config['UPLOAD_FOLDER'],f.filename)
-				us=savefile(path=save_file , department= debartment)
-				db.session.add(us)
-				db.session.commit()
-			return render_template('admin_page.html' , e="successfully")
-		except Exception as e:
-			return render_template('admin_page.html' , e=e)
-	return render_template('admin_page.html' , e='nathing')
-'''
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html'), 404
@@ -559,8 +589,8 @@ def page_not_found(e):
 if __name__ == '__main__':
 	#app.run(debug=True ,ssl_context=('cert.pem', 'key.pem'), host="0.0.0.0")
 	#app.run(debug=True ,ssl_context=('cert.pem', 'key.pem'))
-	app.run(debug=True , host="0.0.0.0")
-	#app.run(debug=True)
+	#app.run(debug=True , host="0.0.0.0")
+	app.run(debug=True)
 	#ipconfig
 	#--------CMD----------
 	#set FLASK_APP=run
